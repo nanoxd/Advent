7c64b8b729f8df2d6a269950e9f7ab067b806647
Fernando Paredes <nano@fdp.io>
2018-12-02 12:11:55 -0500

    Initial Commit

---
+++ AOC/AOC.h
@@ -0,0 +1,19 @@
+//
+//  AOC.h
+//  AOC
+//
+//  Created by Fernando Paredes on 12/2/18.
+//  Copyright © 2018 Pixel Galaxyz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AOC.
+FOUNDATION_EXPORT double AOCVersionNumber;
+
+//! Project version string for AOC.
+FOUNDATION_EXPORT const unsigned char AOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOC/PublicHeader.h>
+
+
---
+++ Advent/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent
+//
+//  Created by Fernando Paredes on 12/2/18.
+//  Copyright © 2018 Pixel Galaxyz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
